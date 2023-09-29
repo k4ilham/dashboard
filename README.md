4810409e2e15bcc0dd21952a21fb7bd71bcce852
user72ilham <143789992+user72ilham@users.noreply.github.com>
2023-09-29 11:06:51 +0700

    update

---
+++ README.md
@@ -0,0 +1,14 @@
+# Dicoding Collection Dashboard ✨
+
+## Setup environment
+```
+conda create --name main-ds python=3.9
+conda activate main-ds
+pip install numpy pandas scipy matplotlib seaborn jupyter streamlit babel
+```
+
+## Run steamlit app
+```
+streamlit run dashboard.py
+```
+
--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
+
 sns.set(style='dark')
+
+# Helper function yang dibutuhkan untuk menyiapkan berbagai dataframe
 
 def create_daily_orders_df(df):
     daily_orders_df = df.resample(rule='D', on='order_date').agg({
@@ -62,19 +65,20 @@ def create_rfm_df(df):
     
     return rfm_df
 
+# Load cleaned data
 all_df = pd.read_csv("all_data.csv")
 
 datetime_columns = ["order_date", "delivery_date"]
 all_df.sort_values(by="order_date", inplace=True)
 all_df.reset_index(inplace=True)
- 
+
 for column in datetime_columns:
     all_df[column] = pd.to_datetime(all_df[column])
 
-
+# Filter data
 min_date = all_df["order_date"].min()
 max_date = all_df["order_date"].max()
- 
+
 with st.sidebar:
     # Menambahkan logo perusahaan
     st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
@@ -89,6 +93,9 @@ with st.sidebar:
 main_df = all_df[(all_df["order_date"] >= str(start_date)) & 
                 (all_df["order_date"] <= str(end_date))]
 
+# st.dataframe(main_df)
+
+# # Menyiapkan berbagai dataframe
 daily_orders_df = create_daily_orders_df(main_df)
 sum_order_items_df = create_sum_order_items_df(main_df)
 bygender_df = create_bygender_df(main_df)
@@ -96,20 +103,21 @@ byage_df = create_byage_df(main_df)
 bystate_df = create_bystate_df(main_df)
 rfm_df = create_rfm_df(main_df)
 
-st.header('Dicoding Collection Dashboard :sparkles:')
 
+# plot number of daily orders (2021)
+st.header('Dicoding Collection Dashboard :sparkles:')
 st.subheader('Daily Orders')
- 
+
 col1, col2 = st.columns(2)
- 
+
 with col1:
     total_orders = daily_orders_df.order_count.sum()
     st.metric("Total orders", value=total_orders)
- 
+
 with col2:
     total_revenue = format_currency(daily_orders_df.revenue.sum(), "AUD", locale='es_CO') 
     st.metric("Total Revenue", value=total_revenue)
- 
+
 fig, ax = plt.subplots(figsize=(16, 8))
 ax.plot(
     daily_orders_df["order_date"],
@@ -120,22 +128,24 @@ ax.plot(
 )
 ax.tick_params(axis='y', labelsize=20)
 ax.tick_params(axis='x', labelsize=15)
- 
+
 st.pyplot(fig)
 
+
+# Product performance
 st.subheader("Best & Worst Performing Product")
- 
+
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
- 
+
 colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
- 
+
 sns.barplot(x="quantity_x", y="product_name", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
 ax[0].set_ylabel(None)
 ax[0].set_xlabel("Number of Sales", fontsize=30)
 ax[0].set_title("Best Performing Product", loc="center", fontsize=50)
 ax[0].tick_params(axis='y', labelsize=35)
 ax[0].tick_params(axis='x', labelsize=30)
- 
+
 sns.barplot(x="quantity_x", y="product_name", data=sum_order_items_df.sort_values(by="quantity_x", ascending=True).head(5), palette=colors, ax=ax[1])
 ax[1].set_ylabel(None)
 ax[1].set_xlabel("Number of Sales", fontsize=30)
@@ -145,16 +155,17 @@ ax[1].yaxis.tick_right()
 ax[1].set_title("Worst Performing Product", loc="center", fontsize=50)
 ax[1].tick_params(axis='y', labelsize=35)
 ax[1].tick_params(axis='x', labelsize=30)
- 
+
 st.pyplot(fig)
 
+# customer demographic
 st.subheader("Customer Demographics")
- 
+
 col1, col2 = st.columns(2)
- 
+
 with col1:
     fig, ax = plt.subplots(figsize=(20, 10))
- 
+
     sns.barplot(
         y="customer_count", 
         x="gender",
@@ -168,12 +179,12 @@ with col1:
     ax.tick_params(axis='x', labelsize=35)
     ax.tick_params(axis='y', labelsize=30)
     st.pyplot(fig)
- 
+
 with col2:
     fig, ax = plt.subplots(figsize=(20, 10))
     
     colors = ["#D3D3D3", "#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
- 
+
     sns.barplot(
         y="customer_count", 
         x="age_group",
@@ -187,7 +198,7 @@ with col2:
     ax.tick_params(axis='x', labelsize=35)
     ax.tick_params(axis='y', labelsize=30)
     st.pyplot(fig)
- 
+
 fig, ax = plt.subplots(figsize=(20, 10))
 colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 sns.barplot(
@@ -204,47 +215,48 @@ ax.tick_params(axis='y', labelsize=20)
 ax.tick_params(axis='x', labelsize=15)
 st.pyplot(fig)
 
+
+# Best Customer Based on RFM Parameters
 st.subheader("Best Customer Based on RFM Parameters")
- 
+
 col1, col2, col3 = st.columns(3)
- 
+
 with col1:
     avg_recency = round(rfm_df.recency.mean(), 1)
     st.metric("Average Recency (days)", value=avg_recency)
- 
+
 with col2:
     avg_frequency = round(rfm_df.frequency.mean(), 2)
     st.metric("Average Frequency", value=avg_frequency)
- 
+
 with col3:
     avg_frequency = format_currency(rfm_df.monetary.mean(), "AUD", locale='es_CO') 
     st.metric("Average Monetary", value=avg_frequency)
- 
+
 fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(35, 15))
 colors = ["#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9"]
- 
+
 sns.barplot(y="recency", x="customer_id", data=rfm_df.sort_values(by="recency", ascending=True).head(5), palette=colors, ax=ax[0])
 ax[0].set_ylabel(None)
 ax[0].set_xlabel("customer_id", fontsize=30)
 ax[0].set_title("By Recency (days)", loc="center", fontsize=50)
 ax[0].tick_params(axis='y', labelsize=30)
 ax[0].tick_params(axis='x', labelsize=35)
- 
+
 sns.barplot(y="frequency", x="customer_id", data=rfm_df.sort_values(by="frequency", ascending=False).head(5), palette=colors, ax=ax[1])
 ax[1].set_ylabel(None)
 ax[1].set_xlabel("customer_id", fontsize=30)
 ax[1].set_title("By Frequency", loc="center", fontsize=50)
 ax[1].tick_params(axis='y', labelsize=30)
 ax[1].tick_params(axis='x', labelsize=35)
- 
+
 sns.barplot(y="monetary", x="customer_id", data=rfm_df.sort_values(by="monetary", ascending=False).head(5), palette=colors, ax=ax[2])
 ax[2].set_ylabel(None)
 ax[2].set_xlabel("customer_id", fontsize=30)
 ax[2].set_title("By Monetary", loc="center", fontsize=50)
 ax[2].tick_params(axis='y', labelsize=30)
 ax[2].tick_params(axis='x', labelsize=35)
- 
-st.pyplot(fig)
- 
-st.caption('Copyright (c) Dicoding 2023')
 
+st.pyplot(fig)
+
+st.caption('Copyright © Dicoding 2023')
